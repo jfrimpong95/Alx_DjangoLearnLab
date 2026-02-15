4c33d0d688cb0cae6ddc507a8baef284400e7a32
Frimpong Joseph <josephfrimpong91@gmail.com>
2026-02-15 21:24:40 +0000

    Implement profile view with login required
    Added user profile view with login requirement.

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+@login_required
+def profile(request):
+    user = request.user
+
+    if request.method == 'POST':
+        # Get form data
+
