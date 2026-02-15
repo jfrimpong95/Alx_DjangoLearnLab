1b0e0012278a2b45b8a45600fe622abecf42b694
Frimpong Joseph <josephfrimpong91@gmail.com>
2026-02-15 22:01:38 +0000

    Create settings.py

---
+++ django_blog/settings.py
@@ -0,0 +1,15 @@
+INSTALLED_APPS = [
+    # Default Django apps...
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Your blog app
+    'blog',
+
+    # Add taggit here
+    'taggit',
+]
