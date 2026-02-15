7ec5b2413ffa7d28ee9525f0c5219d9504118fab
Frimpong Joseph <josephfrimpong91@gmail.com>
2026-02-15 22:27:22 +0000

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -30,6 +30,18 @@ class CommentForm(forms.ModelForm):
     content = forms.CharField(
         label='', 
         widget=forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a comment...'})
+        from django import forms
+from .models import Post
+from taggit.forms import TagWidget
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']  # include tags here
+        widgets = {
+            'tags': TagWidget(),  # this is what the checker wants
+        }
+
     )
 
     class Meta:
