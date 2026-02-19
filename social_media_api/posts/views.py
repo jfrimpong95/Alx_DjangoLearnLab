eb5c54604fa11bf790872ca18070214ef914adf5
Frimpong Joseph <josephfrimpong91@gmail.com>
2026-02-19 14:33:36 +0000

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -22,3 +22,21 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('-created_at')
     serializer_class = Comm_
+
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Post
+from .serializers import PostSerializer
+
+from rest_framework.views import APIView
+
+class FeedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        following_users = user.following.all()
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
