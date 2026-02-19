e3ad41198349fe75b93d4965d32ccda1f12581ff
Frimpong Joseph <josephfrimpong91@gmail.com>
2026-02-19 14:31:18 +0000

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -18,3 +18,28 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(
+        upload_to='profile_pics/',
+        blank=True,
+        null=True
+    )
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='following',
+        blank=True
+    )
+
+    # Optional: you can also define a property for easy access
+    @property
+    def following_users(self):
+        return self.following.all()
+
+    def __str__(self):
+        return self.username
